8029138ffa326c9e35b760ce7a4aa6790dc6894a
dilli <dilli@howdy.do>
2024-03-27 18:48:32 +0530

    new toggle setup

--- src/assets/js/toggleTheme.js
+++ src/assets/js/toggleTheme.js
@@ -1,44 +1,79 @@
-import { themeIcons } from "./themeIcons.js";
-
-const favicon = document.querySelector('link[rel="shortcut icon"]');
-
+var currentTheme = '';
 const html = document.querySelector("html");
-const themeButton = document.querySelector(".theme-button");
+const theme_icon = document.querySelector('.theme-button');
+function themeselect(color) {
+  
+  html.classList.add(color);
+  localStorage.setItem('theme',color);
 
-let currentTheme = localStorage.getItem("theme");
+  var theme = document.createElement('div');
+  theme.classList.remove('theme_icon');
+  theme.classList.add('theme_icon');
+  theme.innerHTML = current_theme_icon(color);
+  theme_icon.append(theme);
 
-function getPreferredTheme() {
-  return window.matchMedia("(prefers-color-scheme: light)").matches ? "light" : "dark";
 }
 
-favicon.href = themeIcons.favicon[getPreferredTheme()];
+function current_theme_icon(type) {
 
-if (currentTheme === "light" || currentTheme === "dark") {
-  html.classList.add(currentTheme);
-  themeButton.innerHTML = themeIcons[currentTheme];
-} else {
-  currentTheme = "auto";
-  localStorage.setItem("theme", currentTheme);
-  themeButton.innerHTML = themeIcons[getPreferredTheme()];
-}
-
-const toggleTheme = () => {
-  if (currentTheme === "light" || currentTheme === "dark") {
-    html.classList.remove(currentTheme);
-    currentTheme = "auto";
-    localStorage.setItem("theme", currentTheme);
-    themeButton.innerHTML = themeIcons[getPreferredTheme()];
+  if (type == 'light') {
+    return `<svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M5 11.4597C5 15.6241 8.4742 19 12.7598 19C16.0591 19 18.8774 16.9993 20 14.1783C19.1109 14.5841 18.1181 14.8109 17.0709 14.8109C13.2614 14.8109 10.1732 11.8101 10.1732 8.1084C10.1732 6.56025 10.7134 5.13471 11.6205 4C7.87502 4.5355 5 7.67132 5 11.4597Z"
+                stroke="#0F172A"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            />
+        </svg>`
   } else {
-    if (getPreferredTheme() === "light") {
-      currentTheme = "dark";
-    } else {
-      currentTheme = "light";
-    }
+    return `<svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M17 12C17 14.7614 14.7614 17 12 17C9.23858 17 7 14.7614 7 12C7 9.23858 9.23858 7 12 7C14.7614 7 17 9.23858 17 12Z"
+                stroke="#0F172A"
+                stroke-width="1.5"
+            />
 
-    localStorage.setItem("theme", currentTheme);
-    html.classList.add(currentTheme);
-    themeButton.innerHTML = themeIcons[currentTheme];
+            <path
+                d="M18.3117 5.68834L18.4286 5.57143M5.57144 18.4286L5.68832 18.3117M12 3.07394V3M12 21V20.9261M3.07394 12H3M21 12H20.9261M5.68831 5.68834L5.5714 5.57143M18.4286 18.4286L18.3117 18.3117"
+                stroke="#0F172A"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            />
+        </svg>`
   }
-};
 
-themeButton.addEventListener("click", toggleTheme);
+}
+
+
+theme_icon.addEventListener("click", () =>{
+  if(localStorage.getItem('theme')=='dark'){
+   
+    themeselect('light')
+  }else{
+    
+   themeselect('dark')
+  }
+});
+
+var current_time = new Date().getHours();
+
+if (current_time > 12) {
+  themeselect('dark');
+} else {
+  themeselect('light');
+}
+// themeselect();
